e8b4d731ecb255cd3143b2b2411854af934a2c3f
Rifqi Fauzan <rifqifauzan2096@gmail.com>
2025-12-15 11:26:03 +0700

    add seed admin

---
+++ database/seed/seed_admin.go
@@ -0,0 +1,27 @@
+package seed
+
+import (
+	"log"
+
+	"github.com/rifqifauzan2096/project-management/config"
+	"github.com/rifqifauzan2096/project-management/models"
+	"github.com/rifqifauzan2096/project-management/utils"
+)
+
+func SeedAdmin(){
+	password, _ := utils.HashPassword("admin123")
+
+	admin := models.User{
+		Name: "Super Admin",
+		Email: "admin@expemail.com",
+		Password: password,
+		Role: "admin",
+	}
+
+	if err := config.DB.FirstOrCreate(&admin, models.User{Email: admin.Email}).Error; err != nil{
+		log.Println("Failed to seed admin", err)
+	} else {
+		log.Println("Admin user seeded")
+	}
+
+}
